ab17e0c06f516ee86728c28f49cf03eed4761101
Md Tohidur Rahman Siam <trsiam3468@gmail.com>
2025-08-22 15:35:46 +0600

    Add files via upload

---
+++ problem_20.py
@@ -0,0 +1,49 @@
+#https://www.hackerrank.com/challenges/text-alignment/problem?isFullScreen=true
+
+# width=10
+# print("Hacker".ljust(width,'-'))
+
+width=int(input())
+i=1
+for i in range(width+1):
+    row="H"*i
+    print(row.center(width*2))
+        
+        
+
+    
+   
+
+    
+
+    
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
